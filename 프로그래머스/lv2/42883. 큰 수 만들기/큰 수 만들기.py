cfe09e2a498a504877ba27d7cc93a6c66dfbb5f2
Yangjaehong <89791775+Yangjaehong@users.noreply.github.com>
2022-10-04 03:44:18 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,42 @@
+def solution(number, k):
+
+    answer = []
+    for i in range(len(number)):
+        
+        if i+k+1 > len(number):
+            tmp = len(number)
+        else:
+            tmp = i+k+1
+        if i+1 == tmp:
+            break
+        
+        if number[i] == '9':
+            continue
+        else:
+            for j in range(i+1,tmp):
+                if number[i] < number[j]:
+                    answer.append(i)
+                    k -= 1
+                    break
+            if k == 0:
+                break
+    
+    if len(answer) == 0:
+        return "".join(number[:-k])
+    
+    c = ''
+    tmp = 0
+
+    j = 0
+    for i in range(len(number)):
+        if tmp == 1:
+            c += number[i]
+        elif answer[j] != i and tmp == 0:
+            c += number[i]
+        else:
+            j+=1
+        
+        if j == len(answer):
+            tmp = 1
+    return c
+                
