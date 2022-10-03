64f812eb99514b17bf48ce8e019ce8d81915ee7c
Yangjaehong <89791775+Yangjaehong@users.noreply.github.com>
2022-10-03 23:36:30 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,23 +1,29 @@
-def solution(n, k):
-    word=""
-    while n:         
-        word = str(n%k)+word
-        n=n//k
-        
-    word=word.split("0") 
-    
-    count=0
-    for w in word:
-        if w == '':    
+def cheak(answer):
+    cnt = 0
+    for m in answer:
+        if len(m) == 0:
             continue
-        if int(w)<2:
+        if int(m) < 2:
             continue
-        sosu=True
-        for i in range(2,int(int(w)**0.5)+1): 
-            if int(w)%i==0:
-                sosu=False
-                break
-        if sosu:
-            count+=1
             
-    return count
+        else:
+            i = int(m)
+            c = True
+            for n in range(2,int(i**0.5) + 1):
+                if i % n == 0:
+                    c = False
+                    break
+            if c:
+                cnt += 1
+    return cnt
+
+
+def solution(n, k):
+    answer = ''
+
+    while n:
+        answer = str(n%k) + answer
+        n //= k
+    answer = answer.split('0')
+
+    return cheak(answer)
