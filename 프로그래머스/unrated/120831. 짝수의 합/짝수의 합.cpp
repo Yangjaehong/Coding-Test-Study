dc19392512b9ab5fca9bbc743da0594856f1abef
Yangjaehong <89791775+Yangjaehong@users.noreply.github.com>
2022-10-04 15:13:16 +0900

    [unrated] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 2; i < n+1; i+=2){
+        answer += i;
+    }
+    return answer;
+}
